d7837409d653625109094c3f5bc9fbfc982c89bf
Fredy Augusto Castellon Bedoya <castellonbedoya@gmail.com>
2025-08-20 09:39:38 -0600

    CH2: Database: Postgres + Goose Migration

---
+++ sql/schema/001_users.sql
@@ -0,0 +1,10 @@
+-- +goose Up
+CREATE TABLE users (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    created_at TIMESTAMPTZ NOT NULL,
+    updated_at TIMESTAMPTZ NOT NULL,
+    name TEXT NOT NULL
+);
+
+-- +goose Down
+DROP TABLE users;
